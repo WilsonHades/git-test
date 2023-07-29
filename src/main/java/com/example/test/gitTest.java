d06fc9202a1052f7bf0a48442684302b08f93230
mingmo <mingmo636@gmail.com>
2023-07-29 16:32:11 +0800

    two commit

--- src/main/java/com/example/test/gitTest.java
+++ src/main/java/com/example/test/gitTest.java
@@ -3,5 +3,6 @@ package com.example.test;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello word");
+        System.out.println("hello word");
     }
 }
